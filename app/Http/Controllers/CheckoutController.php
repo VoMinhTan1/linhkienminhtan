188dbe19dc687e325d8da706f72a59894ddde872
VoMinhTan1 <68368933+VoMinhTan1@users.noreply.github.com>
2021-07-25 00:16:49 +0700

    editcode

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -117,7 +117,7 @@ class CheckoutController extends Controller
                 $vnp_TmnCode = "WOYWPEZE"; //Mã website tại VNPAY 
                 $vnp_HashSecret = "BOYDRBFXDYBLHCAAHUTHAKRFQHLVMJBS"; //Chuỗi bí mật
                 $vnp_Url = "http://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-                $vnp_Returnurl = "http://localhost:80/linhkienweb/Return-Result";
+                $vnp_Returnurl = "http://linhkienminhtan.herokuapp.com/Return-Result";
                 $vnp_TxnRef = date("YmdHis"); //Mã đơn hàng. Trong thực tế Merchant cần insert đơn hàng vào DB và gửi mã này sang VNPAY
                 $vnp_OrderInfo = "Thanh toán hóa đơn phí dich vụ";
                 $vnp_OrderType = 'billpayment';
--- app/Http/Controllers/VnPayController.php
+++ app/Http/Controllers/VnPayController.php
@@ -15,7 +15,7 @@ class VnPayController extends Controller
         $vnp_TmnCode = "867ETWWS"; //Mã website tại VNPAY 
         $vnp_HashSecret = "WIAJFTZTGBTULEORGCGESBOKVTSNZIKW"; //Chuỗi bí mật
         $vnp_Url = "http://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-        $vnp_Returnurl = "http://localhost:8080/weblinhkien/Return-Result";
+        $vnp_Returnurl = "http://linhkienminhtan.herokuapp.com/Return-Result";
         $vnp_TxnRef = date("YmdHis"); //Mã đơn hàng. Trong thực tế Merchant cần insert đơn hàng vào DB và gửi mã này sang VNPAY
         $vnp_OrderInfo = "Thanh toán hóa đơn phí dich vụ";
         $vnp_OrderType = 'billpayment';
